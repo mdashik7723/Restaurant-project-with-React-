efe337c046a3e40ad25e056754b1367fc4d038b5
mdashik7723 <mdashik7723@gmail.com>
2022-06-27 01:41:37 +0600

    --

---
+++ src/Data/dishes.js
@@ -0,0 +1,154 @@
+const DISHES = [
+    {
+        id: 0,
+        name: 'Ginger Burger',
+        image: 'assets/images/burger.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: 'Inspired by a favourite Chinese Canadian ' +
+            'dish hailing from Calgary, this burger is sure to please. Be careful' +
+            ' if you\'ve got your windows open while making these, ' +
+            'you may just have your neighbours knocking, inquiring what\'s for dinner?',
+
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "imagine all the eatable",
+                author: "Bappy",
+                date: "2022-06-27"
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: "imagine all the eatable",
+                author: "Saif",
+                date: "2022-06-26"
+            },
+            {
+                id: 2,
+                rating: 4.5,
+                comment: "imagine all the eatable",
+                author: "Karim",
+                date: "2022-06-24"
+            },
+            {
+                id: 3,
+                rating: 3,
+                comment: "imagine all the eatable",
+                author: "Ashik",
+                date: "2022-06-28"
+            },
+            {
+                id: 4,
+                rating: 5,
+                comment: "imagine all the eatable",
+                author: "Tamim",
+                date: "2022-06-21"
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Pizza',
+        image: 'assets/images/Pizza.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: 'Inspired by a favourite Chinese Canadian ' +
+            'dish hailing from Calgary, this burger is sure to please. Be careful' +
+            ' if you\'ve got your windows open while making these, ' +
+            'you may just have your neighbours knocking, inquiring what\'s for dinner?',
+
+        comments: [
+            {
+                id: 0,
+                rating: 5,
+                comment: "imagine all the eatable",
+                author: "Manna",
+                date: "2022-06-27"
+            },
+            {
+                id: 1,
+                rating: 4,
+                comment: "imagine all the eatable",
+                author: "Zasim",
+                date: "2022-06-26"
+            },
+            {
+                id: 2,
+                rating: 4.5,
+                comment: "imagine all the eatable",
+                author: "Naim",
+                date: "2022-06-24"
+            },
+            {
+                id: 3,
+                rating: 3,
+                comment: "imagine all the eatable",
+                author: "Santo",
+                date: "2022-06-28"
+            },
+            {
+                id: 4,
+                rating: 5,
+                comment: "imagine all the eatable",
+                author: "Shabbir",
+                date: "2022-06-21"
+            },
+            {
+                id: 2,
+                name: 'Hot Dog',
+                image: 'assets/images/hot dog.jpg',
+                category: 'meal',
+                label: 'Hot',
+                price: '199',
+                description: 'Inspired by a favourite Chinese Canadian ' +
+                    'dish hailing from Calgary, this burger is sure to please. Be careful' +
+                    ' if you\'ve got your windows open while making these, ' +
+                    'you may just have your neighbours knocking, inquiring what\'s for dinner?',
+
+                comments: [
+                    {
+                        id: 0,
+                        rating: 5,
+                        comment: "imagine all the eatable",
+                        author: "Alamgir",
+                        date: "2022-06-27"
+                    },
+                    {
+                        id: 1,
+                        rating: 4,
+                        comment: "imagine all the eatable",
+                        author: "Rubel",
+                        date: "2022-06-26"
+                    },
+                    {
+                        id: 2,
+                        rating: 4.5,
+                        comment: "imagine all the eatable",
+                        author: "Kamal",
+                        date: "2022-06-24"
+                    },
+                    {
+                        id: 3,
+                        rating: 3,
+                        comment: "imagine all the eatable",
+                        author: "Proloy",
+                        date: "2022-06-28"
+                    },
+                    {
+                        id: 4,
+                        rating: 5,
+                        comment: "imagine all the eatable",
+                        author: "Kumar",
+                        date: "2022-06-21"
+                    }
+                ]
+            }
+    ]
+    },
+    ]
+
+export default DISHES;
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Menu from "./Menu";
+
+
 const Body = () => {
     return(
         <div>
-
+         <Menu/>
         </div>
     );
 }
