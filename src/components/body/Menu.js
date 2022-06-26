94c6549b8c129f5711b2ba40fcff2066006087c1
mdashik7723 <mdashik7723@gmail.com>
2022-06-27 02:30:18 +0600

    --

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
 import DISHES from '../../Data/dishes';
 import MenuItem from "./MenuItem";
+import DishDetails from "./DishDetails";
 
 
 
 class Menu extends Component {
     state = {
-        dishes: DISHES
+        dishes: DISHES,
+        selectedDish: null
+    }
+    onDishSelect = dish =>{
+        this.setState({selectedDish: dish})
     }
 
     render() {
          const menu = this.state.dishes.map(item => {
             return (
-                <MenuItem dish = {item} key={item.id}/>
+                <MenuItem
+                    dish = {item}
+                    key={item.id}
+                    DishSelect = {() => this.onDishSelect(item)}/>
             );
         })
+
+        let dishDetail = null;
+         if (this.state.selectedDish != null) {
+             dishDetail = <DishDetails dish = {this.state.selectedDish}/>
+         }
+
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-6">
                         {menu}
+                    </div>
+                    <div className="col-6">
+                        {dishDetail}
                     </div>
                 </div>
             </div>
--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -15,7 +15,12 @@ const MenuItem = props => {
                              src={props.dish.image}
                              style={{opacity: "0.5"}}/>
                     <CardImgOverlay>
-                        <CardTitle>{props.dish.name}</CardTitle>
+                        <CardTitle
+                            style={{cursor: "pointer"}}
+                            onClick={props.DishSelect}>
+                            {props.dish.name}
+
+                        </CardTitle>
                     </CardImgOverlay>
 
                 </CardBody>
