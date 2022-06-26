38783be9f6e091e9d669c0387942691d0590bbc8
mdashik7723 <mdashik7723@gmail.com>
2022-06-26 21:27:05 +0600

    --

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import MainComponents from './components/MainComponents';
 
 function App() {
   return (
     <div className="App">
+      <MainComponents/>
 
     </div>
   );
