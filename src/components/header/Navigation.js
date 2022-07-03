e1ca673a3536d20db8f1c3982c42925a5c22c1d6
mdashik7723 <mdashik7723@gmail.com>
2022-07-03 23:58:58 +0600

    --

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,28 +1,44 @@
  import React, {Component} from "react";
-import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, collapse } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse } from 'reactstrap';
  import {Link} from "react-router-dom";
 
 class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            isNavOpen: false
+        }
+    }
+    navToggle = () => {
+        this.setState({
+            isNavOpen: ! this.state.isNavOpen
+        })
+    }
     render() {
         return(
             <div>
-                <Navbar dark color="dark">
-                    <div className="container"><NavbarBrand href="/"> Ashik's Restaurant</NavbarBrand></div>
-                </Navbar>
+                <Navbar dark color="dark" expand ="sm">
+                    <div className="container">
+                        <NavbarToggler onClick={this.navToggle}/>
+                        <NavbarBrand href="/"> Ashik's Restaurant</NavbarBrand>
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
                 <Nav className="mr-auto" navbar>
-                    <nanItem>
+                    <NavItem>
                         <Link to = "/" className = "nav-link active" >Home</Link>
-                    </nanItem>
-                    <nanItem>
+                    </NavItem>
+                    <NavItem>
                         <Link to = "/menu" className = "nav-link active" >Menu</Link>
-                    </nanItem>
-                    <nanItem>
+                    </NavItem>
+                    <NavItem>
                         <Link to = "/about" className = "nav-link active" >About</Link>
-                    </nanItem>
-                    <nanItem>
+                    </NavItem>
+                    <NavItem>
                         <Link to = "/contact" className = "nav-link active" >Contact</Link>
-                    </nanItem>
+                    </NavItem>
                 </Nav>
+            </Collapse>
+            </div>
+    </Navbar>
             </div>
         );
     }
