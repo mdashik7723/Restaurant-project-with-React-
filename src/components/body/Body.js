312a60ac743978e2206fc8c4a47d1623be6c9bd2
mdashik7723 <mdashik7723@gmail.com>
2022-07-04 02:20:36 +0600

    --

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,16 +3,16 @@ import Menu from "./Menu";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
-import {Routes} from "react-router";
+import {Routes, Redirect } from "react-router-dom";
 
 
 const Body = () => {
     return (
         <div>
-            <Routes path="/" exact component={Home}/>
-            <Routes path="/Menu" exact component={Menu}/>
-            <Routes path="/contact" exact component={Contact}/>
-            <Routes path="/about" exact component={About}/>
+            <Routes path="/" exact component={<Home/>}/>
+            <Routes path="/menu" exact component={<Menu/>}/>
+            <Routes path="/contact" exact component={<Contact/>}/>
+            <Routes path="/about" exact component={<About/>}/>
         </div>
     );
 }
